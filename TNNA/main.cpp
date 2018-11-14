9cc88d9a5f81791937203ba12a179dcc4111cf9f
Mapoet <Mapoet.Niphy@Gmail.com>
2018-11-14 13:54:52 +0800

    Initial Commit

---
+++ TNNA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TNNA
+//
+//  Created by Mapoet Niphy on 2018/11/14.
+//  Copyright © 2018 Mapoet Niphy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
